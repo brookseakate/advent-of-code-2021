e903ccc7c1179d2e688d58b530311f6f7afbb7b6
Kate Shaffer <kate.shaffer@gmail.com>
2021-12-02 09:20:43 -0800

    Add Day 2 part 2

--- src/com/aoc2021/day2/DayTwo.kt
+++ src/com/aoc2021/day2/DayTwo.kt
@@ -6,7 +6,32 @@ import java.nio.file.Paths
 class DayTwo {
   companion object {
     fun main() {
-      println(partOne())
+      println(partTwo())
+    }
+
+    private fun partTwo(): Int {
+      var aim = 0
+      var depth = 0
+      var horizontalPosition = 0
+
+      val inputLines: MutableList<String> = Files.readAllLines(Paths.get("./src/com/aoc2021/day2/Input"))
+
+      for (line: String in inputLines) {
+        val instruction = line.split(" ")
+        val direction = instruction[0]
+        val distance = instruction[1].toInt()
+
+        when (direction) {
+          "forward" -> {
+            horizontalPosition += distance
+            depth += aim * distance
+          }
+          "down" -> aim += distance
+          "up" -> aim -= distance
+        }
+      }
+
+      return depth * horizontalPosition
     }
 
     private fun partOne(): Int {
