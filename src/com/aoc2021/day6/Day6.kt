4b3eb713596af63eed0022a78d9d1685128c3a6a
Kate Shaffer <kate.shaffer@gmail.com>
2021-12-06 07:55:05 -0800

    Day 6, part 2 (won't work, but see what happens...OOM)

--- src/com/aoc2021/day6/Day6.kt
+++ src/com/aoc2021/day6/Day6.kt
@@ -7,29 +7,29 @@ import com.aoc2021.util.Utils.Companion.readFileAsMutableList
  */
 class Day6 {
   companion object {
-    lateinit var lanternFishPopulation: MutableList<Int>
+    lateinit var lanternFishPopulation: MutableList<Long>
 
     fun main() {
       val input = readFileAsMutableList("day6/Input")
       lanternFishPopulation = input
         .first()
         .split(",")
-        .map { it.toInt() }
+        .map { it.toLong() }
         .toMutableList()
 
-      println(partOne())
+      println(partOneAndTwo(256))
     }
 
-    fun partOne(
-//    population: MutableList<Int>
-    ): Int {
-      for (day in 1..80) {
-        var updatedPop = mutableListOf<Int>()
+    private fun partOneAndTwo(
+      dayCount: Int // partOne: 80, partTwo: 256
+    ): Long {
+      for (day in 1..dayCount) {
+        var updatedPop = mutableListOf<Long>()
 
         lanternFishPopulation.forEachIndexed { i, fish ->
           if (fish > 0) {
             updatedPop.add(fish - 1)
-          } else if (fish == 0) {
+          } else if (fish == 0L) {
             // this modifies initial order here, we don't care about order (at least right now)
             updatedPop.add(6)
             updatedPop.add(8)
@@ -39,7 +39,7 @@ class Day6 {
         lanternFishPopulation = updatedPop
       }
 
-      return lanternFishPopulation.size
+      return lanternFishPopulation.size.toLong() // this obv won't work, but force compilation to see what happens
     }
   }
 }
