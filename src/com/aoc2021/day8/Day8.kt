1af2beb69cf8d74af062d43d359043da9d3022f7
Kate Shaffer <kate.shaffer@gmail.com>
2021-12-08 21:19:35 -0800

    Add TODOs for Day 8, part 2

--- src/com/aoc2021/day8/Day8.kt
+++ src/com/aoc2021/day8/Day8.kt
@@ -41,6 +41,37 @@ class Day8 {
         .size
     }
 
+    private fun partTwo(
+      patterns: List<Pattern>
+    ): Int {
+      // figure out the patterns
+      // map the output vals -> digitStrings -> ints
+      // sum output vals & return
+    }
+
+    private fun figureOutNumbers(
+      sigPatterns: List<String>
+    ): List<String> {
+      // index = digit
+      val digitStrings = MutableList(10) { "" }
+
+      // TODO
+      // if length 2: 1
+      // if length 3: 7
+      // l 4: 4
+      // l 7: 8
+      // l 5:
+      // if contains 7's letters: 3
+      // else if contains 3 of 4's letters: 5
+      // else: 2
+      // l 6:
+      // if not both of 1's letters: 6
+      // if not all of 4's letters: 0
+      // else: 9
+
+      return digitStrings
+    }
+
     private fun readInputAsPatterns(
       input: MutableList<String>
     ): List<Pattern> /* Pattern = Pair<List<String>, List<String>> */ {
