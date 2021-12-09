a43c7213c46b8892b94932c9b4e0be81b0c7a927
Kate Shaffer <kate.shaffer@gmail.com>
2021-12-08 22:44:39 -0800

    Day 8, part 2

--- src/com/aoc2021/day8/Day8.kt
+++ src/com/aoc2021/day8/Day8.kt
@@ -21,7 +21,8 @@ class Day8 {
 //      val input = readFileAsMutableList("day8/ExampleInput")
       val patterns = readInputAsPatterns(input)
 
-      println(partOne(patterns))
+//      println(partOne(patterns))
+      println(partTwo(patterns))
     }
 
     private fun partOne(
@@ -44,9 +45,31 @@ class Day8 {
     private fun partTwo(
       patterns: List<Pattern>
     ): Int {
-      // figure out the patterns
-      // map the output vals -> digitStrings -> ints
-      // sum output vals & return
+      var total = 0
+      for (pattern in patterns) {
+        val digitStrings = figureOutNumbers(pattern.first)
+
+        total += mapOutputVals(
+          digitStrings = digitStrings,
+          outputVals = pattern.second
+        )
+      }
+
+      return total
+    }
+
+    private fun mapOutputVals(
+      digitStrings: List<String>,
+      outputVals: List<String>
+    ): Int {
+      var outputString = ""
+
+      for (oVal in outputVals) {
+        val matchingDigit = digitStrings.find { it == oVal.toCharArray().sorted().joinToString("") }!!
+        outputString += digitStrings.indexOf(matchingDigit).toString()
+      }
+
+      return outputString.toInt()
     }
 
     private fun figureOutNumbers(
@@ -55,21 +78,57 @@ class Day8 {
       // index = digit
       val digitStrings = MutableList(10) { "" }
 
-      // TODO
-      // if length 2: 1
-      // if length 3: 7
-      // l 4: 4
-      // l 7: 8
-      // l 5:
-      // if contains 7's letters: 3
-      // else if contains 3 of 4's letters: 5
-      // else: 2
-      // l 6:
-      // if not both of 1's letters: 6
-      // if not all of 4's letters: 0
-      // else: 9
+      // get unique-number digitStrings
+      // if length matches, set
+      digitStrings[1] = sigPatterns.find { it.length == SignalDisplay.ONE.segmentQuantity }!!
+      digitStrings[4] = sigPatterns.find { it.length == SignalDisplay.FOUR.segmentQuantity }!!
+      digitStrings[7] = sigPatterns.find { it.length == SignalDisplay.SEVEN.segmentQuantity }!!
+      digitStrings[8] = sigPatterns.find { it.length == SignalDisplay.EIGHT.segmentQuantity }!!
 
-      return digitStrings
+      val fiveSegmentStrings = sigPatterns.filter { it.length == 5 }
+
+      for (string in fiveSegmentStrings) {
+        val splitString = string.split("")
+        if (
+          splitString
+            .containsAll(
+              digitStrings[7].split("")
+            )
+        ) {
+          // if contains 7's letters: 3
+          digitStrings[3] = string
+        } else if (
+          splitString
+            .filter {
+              it in digitStrings[4]
+            }.size == 5 // 3, plus leading/trailing "" I'm too lazy to remove
+        ) {
+          // else if contains 3 of 4's letters: 5
+          digitStrings[5] = string
+        } else {
+          // else: 2
+          digitStrings[2] = string
+        }
+
+        val sixSegmentStrings = sigPatterns.filter { it.length == 6 }
+
+        for (string in sixSegmentStrings) {
+          val splitString = string.split("")
+
+          if (!splitString.containsAll(digitStrings[1].split(""))) {
+            // if not both of 1's letters: 6
+            digitStrings[6] = string
+          } else if (!splitString.containsAll(digitStrings[4].split(""))) {
+            // if not all of 4's letters: 0
+            digitStrings[0] = string
+          } else {
+            // else: 9
+            digitStrings[9] = string
+          }
+        }
+      }
+
+      return digitStrings.map { it.toCharArray().sorted().joinToString("") }
     }
 
     private fun readInputAsPatterns(
